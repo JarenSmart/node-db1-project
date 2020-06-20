6458e364318bc94c520aee87ae567fb212690a09
jaren <jarensmart@yahoo.com>
2020-06-20 17:43:32 -0400

    added POST && PUT endpoints for accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -3,32 +3,74 @@ const db = require("../data/dbConfig");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/accounts", async (req, res, next) => {
   try {
     //translates to 'SELECT * FROM "accounts";
-    const names = await db.select("*").from("accounts");
-    res.json(names);
+    const allAccounts = await db.select("*").from("accounts");
+    res.json(allAccounts);
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  //translates to SELECT * FROM "name" WHERE "id" = ?;
+router.get("/accounts/:id", async (req, res, next) => {
   try {
-    const nameByID = await db
+    // translates to SELECT * FROM "accounts" WHERE "id" = ? LIMIT 1;
+    // destructure the variable below "[nameByID]", since we only want the first index of the array.
+    const [nameByID] = await db
       .select("*")
       .from("accounts")
-      .where("id", req.params.id);
+      .where("id", req.params.id)
+      // The '.limit' allows us to make sure we're only getting a single result, since we're destructuring the array above.
+      .limit(1);
+
     res.json(nameByID);
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", (req, res, next) => {});
+router.post("/accounts", async (req, res, next) => {
+  try {
+    const payload = {
+      // the database will auto generate ID
+      name: req.body.name,
+      budget: req.body.budget,
+    };
 
-router.put("/", (req, res, next) => {});
+    // translates to INSERT INTO "accounts" ("name", "budget") VALUES (?, ?);
+    const [addAccountID] = await db.insert(payload).into("accounts");
+    const addAccount = await db
+      .first("*")
+      .from("accounts")
+      .where("id", addAccountID);
+    //calling ".first()" is doing the same thing as ".limit(1)" and destructuring the result
+    res.status(201).json(addAccount);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/accounts/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      // the database will auto generate ID
+      name: req.body.name,
+      budget: req.body.budget,
+    };
+
+    // translates to 'UPDATE "accounts" SET ? = ? WHERE "id" = ?';
+    await db("accounts").update(payload).where("id", req.params.id);
+    const updateAccount = await db
+      .first("*")
+      .from("accounts")
+      .where("id", req.params.id);
+    //calling ".first()" is doing the same thing as ".limit(1)" and destructuring the result
+    res.json(updateAccount);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.delete("/", (req, res, next) => {});
 
